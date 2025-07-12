6d28f5d2628f07f6d02b6bea933b09df328c414d
tauz001 <tauzans609@gmail.com>
2025-07-12 12:54:37 +0530

    updates in project card

--- ReactPortfolio/src/components/projectSection.jsx
+++ ReactPortfolio/src/components/projectSection.jsx
@@ -3,16 +3,16 @@ import ProjectItem from "./projectItem"
 
 const ProjectSection = ({projects}) => {
   return (
-    <section id="projects" className="section-container">
-      <h2 className="section-title">My Projects</h2>
+      <section id="projects" className="section-container">
+        <h2 className="section-title">My Projects</h2>
 
-      <div className="projects-grid" id="projects-container">
-        {/* <!-- Project cards will be added dynamically with JavaScript --> */}
-        {projects.map((project, index) => (
-          <ProjectItem key={project.id || index} project={project} />
-        ))}
-      </div>
-    </section>
+        <div className="projects-grid" id="projects-container">
+          {/* <!-- Project cards will be added dynamically with JavaScript --> */}
+          {projects.map((project, index) => (
+            <ProjectItem key={project.id || index} project={project} />
+          ))}
+        </div>
+      </section>
   )
 }
 
